03a184f999dbb251fce4abca4e68c49139c17265
foolishandi <10881771+foolishandi@user.noreply.gitee.com>
2022-05-06 05:07:59 +0800

    pull

--- js/global_ex.js
+++ js/global_ex.js
@@ -22,7 +22,234 @@ function checkCookie(key, fn1, fn2) {
         fn1().call(null, data);
     } else {
         fn2().call(null, data);
-
     }
 }
-//ajax
+//歌曲列表
+function songList(song, index) {
+    var tr = $('<tr></tr>');
+    tr.attr('data-id', song.id)
+    var td1 = $('<td>' + index + '</td>');
+    var td2 = $('<td><img><span></span><span></span><i></i></td>')
+    td2.children('img').attr('src', song.pic).attr('data-id', song.id);
+    td2.children('i').attr('class', "iconfont icon-circle-next mx-1").attr('data-id', song.id);
+    td2.children('span').eq(0).attr('class', 'song').html(song.name).attr('data-id', song.id);
+    td2.children('span').eq(1).attr('class', 'res').html(song.al).attr('data-id', song.id);
+    var td3 = $('<td>' + song.time + '</td>')
+    var td4 = $('<td>' + song.singer + '<i class="iconfont icon-add mx-2 my-2 "></i></td>')
+    td1.attr('data-id', song.id);
+    td2.attr('data-id', song.id);
+    td3.attr('data-id', song.id).attr('data-time', song.time).attr('class', 'dataTime');
+    td4.attr('data-name', song.name).attr('class', 'dataName').attr('data-id', 'a' + song.id);
+    tr.append(td1, td2, td3, td4);
+    if (song.fee == 1 || song.fee == 4) {
+        td1.append('--vip--')
+    }
+    return tr;
+}
+
+//歌曲搜索
+
+async function check(id) {
+    await $.get('https://mu-two.vercel.app/check/music?id=' + id, e => {
+        return e.message;
+    })
+}
+// 
+//搜索功能
+//
+function serchSongs(keywords) {
+
+    let p = new Promise((v, f) => {
+        $.post('https://mu-two.vercel.app/cloudsearch?keywords=' + keywords + '&limit=100', e => {
+            v(e);
+        });
+    })
+    return p;
+}
+
+function getSong(data) {
+    let p = new Promise((v, f) => {
+        // console.log(data)
+        var songs = [];
+        var songs2 = {};
+        for (let x of data.result.songs) {
+            var song = {};
+            time = x.dt;
+            song.time = timeFormat(time, 0, 1)
+            song.id = x.id;
+            song.fee = x.fee;
+            song.al = x.al.name;
+            for (let j of x.ar) {
+                song.singerID = j.id;
+                break;
+            }
+            song.name = x.name;
+            song.pic = x.al.picUrl;
+            for (let y of x.ar) {
+                song.singer += y.name + '/'
+            }
+            song.singer = song.singer.replace('undefined', '').replace(/\/$/g, '');
+            songs.push(song)
+            songs2[song.id] = song;
+        }
+        v(songs)
+        sessionStorage.setItem('songs2', JSON.stringify(songs2))
+    });
+    return p;
+}
+
+function serch(keywords) {
+    serchSongs(keywords).then(data => {
+        return getSong(data);
+
+    }).then(data => {
+        $('.load2').addClass('waiting')
+        for (let x in data) {
+            var song = songList(data[x], 1 + eval(x));
+
+            $('.main tbody').append(song)
+        }
+    });
+};
+//时间格式化
+function timeFormat(time, n = 0) {
+    var m, s;
+    var count = Math.pow(10, n);
+    if (typeof time == "number") {
+        m = Math.floor(time / 1000 / 60);
+        s = Math.round((time / 1000 % 60) * count) / count;
+    } else {
+        m = time.split(':')[0];
+        s = time.split(':')[1];
+        s = Math.round(s * count) / count;
+    }
+    m = String(m);
+    s = String(s);
+    if (m.length == 1) {
+        m = '0' + m;
+    }
+    if (Number(m) > 59) {
+        m = Math.floor(m / 60) + ':' + (m % 60)
+    }
+    if (s.length == 1) {
+        s = '0' + s;
+    }
+    s = String(s);
+    if (s.includes('.')) {
+        s1 = s.split('.')[0];
+        s2 = s.split('.')[1];
+        if (s2.length < n) {
+            var j = n - s2.length;
+            for (let i = 1; i <= j; i++) {
+                s2 += '0'
+            }
+        }
+        s = s1 + '.' + s2;
+    } else {
+        if (n != 0) {
+            s = s + '.';
+            for (let i = 1; i <= n; i++) {
+                s += '0'
+            }
+        }
+
+    }
+    return m + ':' + s;
+}
+//tbody
+var load2 = "";
+load2 += "<div class=\"load2\">";
+load2 += "<span class=\"loading\"><\/span>";
+load2 += "<span class=\"loading\"><\/span>";
+load2 += "<span class=\"loading\"><\/span>";
+load2 += "<span class=\"loading\"><\/span>";
+load2 += "<span class=\"loading\"><\/span>";
+load2 += "<span class=\"loading\"><\/span>";
+load2 += "<\/div>";
+//相似歌曲
+function getSimSongs(id) {
+    let p = new Promise((v, f) => {
+        $.post('https://mu-two.vercel.app/simi/song?id=' + id + '&limit=100', e => {
+            v(e);
+        });
+    })
+    return p;
+}
+
+function simsongs(id) {
+    let q = getSimSongs(id).then(data => {
+        let p = new Promise((v, f) => {
+            var songs = [];
+            var songs3 = {};
+            for (let x of data.songs) {
+                var song = {};
+                time = x.bMusic.playTime;
+                song.time = timeFormat(time, 0, 1)
+                song.size = x.bMusic.size + "kb";
+                song.id = x.id;
+                song.singerID = x.artists[0].id;
+                song.fee = x.fee;
+                song.al = x.album.name;
+                song.name = x.name;
+                song.pic = x.album.blurPicUrl;
+                song.url = x.mp3Url;
+
+                song.singer = x.artists[0].name;
+                songs.push(song)
+                songs3[song.id] = song;
+            }
+            sessionStorage.setItem('songs3', JSON.stringify(songs3))
+            v(songs3);
+        })
+        return p;
+    })
+    return q;
+};
+
+//获取歌手mv
+function getMV(singerID) {
+    let p = new Promise((v, f) => {
+        $.post('https://mu-two.vercel.app/artist/mv?id=' + singerID, e => {
+            v(e);
+        });
+    }).then(e => {
+        console.log(e)
+        var mvlist = {};
+        let p = new Promise((v, f) => {
+            var k = e.mvs;
+            for (let x in k) {
+                var mvs = {}
+                mvs.id = k[x].id;
+                mvs.pic = k[x].imgurl;
+                mvs.singer = k[x].artistName;
+                mvs.name = k[x].name;
+                mvs.publish = k[x].publishTime;
+                mvs.playcount = k[x].playCount;
+                mvlist[mvs.id] = mvs;
+            }
+            v(mvs);
+            sessionStorage.setItem('mvlist', JSON.stringify(mvlist))
+        })
+        return p;
+    }).then(data => {
+        var e = JSON.parse(sessionStorage.getItem('mvlist'));
+        for (let x in e) {
+            var infor = $('<span class="mx-3"></span>').html(e[x].name + '-' + e[x].singer + '/' + e[x].publish);
+            var img = $('<img >').attr('src', e[x].pic);
+            var li = $('<li data-bs-toggle="offcanvas" data-bs-target="#mvplayer"></li>').append(img, infor);
+            li.attr('data-id', x)
+            $('ul.mv').append(li);
+        }
+    })
+
+}
+//打开mv
+function openMV(mvid) {
+    let p = new Promise((v, f) => {
+        $.post('https://mu-two.vercel.app/mv/url?id=' + mvid, e => {
+            v(e.data.url);
+        });
+    })
+    return p;
+}
+openMV(5436712)
--- js/index.js
+++ js/index.js
@@ -4,16 +4,164 @@ $(document).ready(function() {
     //
     var qq = JSON.parse(localStorage.getItem("17628097136"))
     var domain = 'https://mu-two.vercel.app'
-    var testMusic = "http://m8.music.126.net/20220504225434/1cb78f06410d3c14ad1c9a9677789e20/ymusic/0fd6/4f65/43ed/a8772889f38dfcb91c04da915b301617.mp3";
+    var testMusic = "http://m702.music.126.net/20220505002924/c133d7e76d240d9a72a863e4457755d6/jd-musicrep-ts/4600/a5fa/c85f/555d31856cd048d0635a8903e3fdce2c.mp3";
 
-    var testMusic2 = './imgs/1.mp3'
-        // 
-        //播放器处理
-        //
+
+    $('#serch').bind('keyup', e => {
+        var key0 = $('#serch').val();
+        if (e.key == 'Enter' && key0 != '') {
+            $('.main table').removeClass('hide');
+            $('tbody').empty();
+            $('tbody').append(load2)
+            sessionStorage.removeItem('songs2');
+            sessionStorage.removeItem('lyric');
+            $('.song-index').html(key0)
+            serch(key0)
+        }
+    })
+
+    // 
+    //点击播放功能
+    //
+    $('.main').on('click', 'td', function() {
+        var _id = $(this).attr('data-id');
+        songPlayer(_id);
+        lyrics(_id)
+    })
+
+    function songPlayer(id, type = false) {
+        $('ul.similar').empty();
+        $('ul.mv').empty();
+        var ad = '#a' + id;
+        $('.detail').attr('data-id', id);
+        if ($(ad).length == 0 || type) {
+            var detailURL = domain + '/song/url?id=' + id;
+            var song = JSON.parse(sessionStorage.getItem('songs2'))[id] ? JSON.parse(sessionStorage.getItem('songs2'))[id] : JSON.parse(sessionStorage.getItem('songs3'))[id];
+            sessionStorage.removeItem('songs3');
+            //歌手mv
+            getMV(song.singerID)
+                //歌手mv
+            function p1(x) {
+                let p = new Promise((v, f) => {
+                    $.post(x, e => {
+                        // console.log(e)
+                        v(e);
+                    });
+                })
+                return p;
+            }
+            p1(detailURL).then(e => {
+                var url = e.data[0].url;
+                if (!url) {
+                    url = 'https://music.163.com/song/media/outer/url?id=' + id + '.mp3'
+                }
+                $('#music').attr('src', url);
+
+                $('img.port').attr('src', song.pic)
+                $('section.detail').children('span').eq(0).html(song.name)
+                $('section.detail').children('span').eq(1).html(song.singer)
+                $('section.detail').children('span').eq(2).html(song.al)
+                $('.allTime').html(song.time)
+                if ($(ad).length == 0) { addMeun(id); }
+
+                if (type) {
+                    $('i.play').click();
+                    $('i.play').click();
+
+                } else {
+                    $('i.play').click();
+                }
+
+
+                //设置当前正在播放
+                $('.meun ul li').removeClass('on');
+                $('.meun ul li[data-id=' + id + ']').toggleClass('on text-light')
+            })
+        } else {
+            $('i.play').click();
+
+        }
+        //相似歌曲
+        simsongs(id).then(e => {
+            for (let x in e) {
+
+                var infor = $('<span class="mx-3"></span>').html(e[x].name + '-' + e[x].singer + '/' + e[x].al);
+                var img = $('<img >').attr('src', e[x].pic);
+                var li = $('<li></li>').append(img, infor);
+                li.attr('data-id', x)
+                $('ul.similar').append(li);
+            }
+        })
+
+    }
+    //歌单部分
+    function addMeun(id) {
+        var total = $('.meun ul li').length;
+        var song = JSON.parse(sessionStorage.getItem('songs2'))[id];
+        var tr = '<li class="list-group-item bg-ligth text-black-50" id=a' + id + ' data-id=' + id + ' index=' + total + '>' + song.name + ' -' + song.singer + ' /' +
+            song.time + ' <span class = "badge bg-secondary del float-end " > x </span></li> '
+        $('.meun ul').append(tr);
+        $('.count.badge').html($('.meun ul li').length - 1)
+    };
+    $('.meun').on('click', 'li', function() {
+        var _id = $(this).attr('data-id');
+        // console.log(_id)
+        songPlayer(_id, true);
+        lyrics(_id)
+    })
+    $('.clear-all').on('click', function() {
+        $('.meun ul').empty().append('<li class="clear-all list-group-item">清空</li>');
+        $('i.pre').click();
+
+    })
+    $('.meun').on('click', 'span', function() {
+        $(this).parent().remove();
+        $('.count.badge').html($('.meun ul li').length - 1)
+
+        return false;
+    })
+    $('.main').on('click', 'i', function() {
+        var _id = $(this).parent().attr('data-id').replace('a', '');
+        // console.log(_id)
+        addMeun(_id);
+        return false;
+    })
+
+
+
+
+    //解析歌词成数组
+    function lyrics(id) {
+        var lrURL = domain + '/lyric?id=' + id;
+
+        function p2(x) {
+            let p = new Promise((v, f) => {
+                $.post(x, e => {
+                    v(e);
+                });
+            })
+            return p;
+        }
+        p2(lrURL).then(e => {
+            var lyric = JSON.stringify(e.lrc.lyric)
+            var lyrics = {};
+            var lyrics_time = [];
+            var lyrics_words = [];
+            var times = lyric.match(/([0-9\.:]{7,9}|(?<=]).*?(?=\\))/g);
+            for (let x in times) {
+                if (x % 2 == 0) {
+                    lyrics[Math.floor(x / 2)] = [timeFormat(times[x]), times[eval(x) + 1]]
+                }
+            }
+            sessionStorage.setItem('lyric', JSON.stringify(lyrics))
+        })
+    }
+    //播放器功能
+    var prePlayer = '';
+    var nextPlay = '';
+    var nowPlay = './imgs/1.mp3'
     var $music = $('#music');
-
-
-    $music.attr('src', testMusic);
+    $music.attr('src', nowPlay);
     $('i.play').click(function() {
         if (document.querySelector('i.pause')) {
             $('#music').get(0).pause();
@@ -27,15 +175,108 @@ $(document).ready(function() {
         };
     });
     $('i.pre').click(() => {
-            if ($('#music').get(0).currentTime > 0) {
-                $('#music').get(0).pause();
-                $('#music').get(0).currentTime = 0;
-                $('i.play').addClass('icon-1_music81').removeClass('icon-music_pause').removeClass('pause');
-                $('img.port').removeClass('active');
-
-            } else {
-
-            };
+        if ($('.pre0').length == 0) {
+            $('#music').get(0).pause();
+            $('#music').get(0).currentTime = 0;
+            $('i.play').addClass('icon-1_music81').removeClass('icon-music_pause').removeClass('pause');
+            $('img.port').removeClass('active');
+            $('i.pre').addClass('pre0')
+        } else {
+            var index = $('.on').attr('index');
+            $('i.pre').removeClass('pre0')
+            var _id = $('li[index=' + (index - 1) + ']').attr('data-id');
+            if (index > 1) {
+                songPlayer(_id, true);
+                lyrics(_id)
+            }
+        };
+    });
+    $('i.next').click(() => {
+            var index = $('.on').attr('index');
+            var _id = $('li[index=' + (eval(index) + 1) + ']').attr('data-id');
+            if (!_id) {
+                _id = $('li[index="1"]').attr('data-id');
+            }
+            // console.log(_id)
+            songPlayer(_id, true);
+            lyrics(_id)
+        })
+        //
+        //拖动音乐进度
+        //
+    $('span.lrics').on('mousedown', function(e) {
+            var ratio = e.offsetX / $('span.lrics').get(0).offsetWidth;
+            $('.progress').css('width', ratio * 100 + '%')
+            $('#music').get(0).currentTime = $('#music').get(0).duration * ratio;
+        })
+        //
+        //音乐进度更新
+    var mus = document.getElementById('music');
+    mus.ontimeupdate = function() {
+        var t1 = mus.currentTime;
+        t1 = Number(t1);
+        var t2 = timeFormat(t1 * 1000)
+        $('span.now').html(t2);
+        $('.lrics .progress').css('width', mus.currentTime / mus.duration * 100 + '%');
+        var lr = JSON.parse(sessionStorage.getItem('lyric'));
+        if (lr) {
+            var c = '';
+            for (let x in lr) {
+                if (lr[x][0] == t2) {
+                    c = x;
+                    break;
+                }
+            }
+            try {
+                $('.nowLyric').html(lr[c][1]);
+                if (c > 1) {
+                    $('p.l1').html(lr[c - 2][1]);
+                    $('p.l2').html(lr[c - 1][1]);
+                }
+                $('p.l3').html(lr[c][1]);
+                $('p.l4').html(lr[eval(c) + 1][1]);
+                $('p.l5').html(lr[eval(c) + 2][1]);
+            } catch (error) {}
+        }
+        if (t1 == mus.duration) {
+            mus.currentTime = 0;
+            $('i.next').click();
+        }
+    }
+    $('.voice').attr('value', '50').on('change', function() {
+            $('#music').get(0).volume = $('.voice').val() / 100;
+        })
+        // 
+        //相似歌曲
+        //
+    $('ul.similar').on('click', 'li', function() {
+            var _id = $(this).attr('data-id');
+            songPlayer(_id, true);
+            lyrics(_id)
+        })
+        //
+        //mv
+        //
+    $('ul.mv').on('click', 'li', function() {
+        var _id = $(this).attr('data-id');
+        $('ul.mvplayer').empty();
+        openMV(_id).then(k => {
+            $('video').attr('src', k)
+            var e = JSON.parse(sessionStorage.getItem('mvlist'));
+            for (let x in e) {
+                var infor = $('<span class="mx-3"></span>').html(e[x].name + '-' + e[x].singer + '/' + e[x].publish);
+                var img = $('<img >').attr('src', e[x].pic);
+                var li = $('<li ></li>').append(img, infor);
+                li.attr('data-id', x)
+                $('ul.mv-player').append(li);
+            }
+        })
+    })
+    $('ul.mv-player').on('click', 'li', function() {
+            var _id = $(this).attr('data-id');
+            openMV(_id).then(k => {
+                $('video').attr('src', k)
+            })
         })
         // 
         //登录处理
@@ -75,10 +316,12 @@ $(document).ready(function() {
 
     function swi() {
         if (document.body.clientWidth <= 992) {
-            $('.bar-top').css('display', 'none')
+            $('.switch').css('display', 'flex');
+            $('.bar-top').css('display', 'none');
             $('#not-offcanvasLeft').attr('id', 'offcanvasLeft')
         } else {
-            $('.bar-top').css('display', 'flex')
+            $('.switch').css('display', 'none');
+            $('.bar-top').css('display', 'flex');
             $('#offcanvasLeft').attr('id', 'not-offcanvasLeft')
         }
     }
@@ -86,5 +329,10 @@ $(document).ready(function() {
     $(window).resize(function() {
         swi();
     })();
+
+
+    //测试
+
+
 
 })
